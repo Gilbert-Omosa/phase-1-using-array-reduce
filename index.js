9261ad60565d0db0f99c6960f2590f8fd3675431
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-27 07:45:43 +0300

    Done With Assignment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// use the .recude function to aggregate the sum total of batteries
+const totalBatteries = batteryBatches.reduce((sumAmount, currentAmount) => {
+  return sumAmount + currentAmount;
+},
+
+// the initial value for total number is 0 and then it accumulates the amounts
+0); 
+
+console.log('Total batteries:', totalBatteries);
